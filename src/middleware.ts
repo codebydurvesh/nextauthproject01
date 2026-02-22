59fae4e84e838d30676ec2f275388aa22ae49f15
codebydurvesh <durvesh.gaikwad08@gmail.com>
2026-02-22 12:50:47 +0530

    removed the middlemare effect from the signup page

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,5 @@ export function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/login", "/signup", "/profile"],
+  matcher: ["/login", "/profile"],
 };
